2ebb9b99a089273036a507aaf071ee3de0061bc0
Adrian Thomas <adrian.l.thomas@outlook.com>
2017-10-24 12:08:06 -0500

    adds app.sass and adds node-sass package

--- basic-redux-boilerplate/src/components/app.js
+++ basic-redux-boilerplate/src/components/app.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import styles from './app.sass';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>hello wonderful world!</h1>
+        <h1>hello <span className={styles.wonderful}>wonderful</span> world!</h1>
       </div>
     )
   }
