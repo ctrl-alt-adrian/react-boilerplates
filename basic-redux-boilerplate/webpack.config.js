586dba2c6fc326486d18b7da7f3c3d4910216b5e
Adrian Thomas <adrian.l.thomas@outlook.com>
2017-10-23 22:42:18 -0500

    updates webpack config

--- basic-redux-boilerplate/webpack.config.js
+++ basic-redux-boilerplate/webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -6,17 +7,25 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   filename: 'index.html',
   inject: 'body'
 })
+
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve('dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'index_bundle.js'
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.(js|jsx|es6)$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.(sass|scss)$/, use: 'style-loader'},
+      { test: /\.(sass|scss)$/, loader: 'css-loader',
+        query: { modules: true, localIdentName: '[name]_[local]_[hash:base64:5]',
+          sourceMap: true, url: false,}
+      },
+      { test: /\.(sass|scss)$/, use: 'sass-loader'}
     ]
   },
+  resolve: { extensions: ['.js', '.jsx']},
+  devServer: { historyApiFallback: true, contentBase: './'},
   plugins: [HtmlWebpackPluginConfig] 
 }
