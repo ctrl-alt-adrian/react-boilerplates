6651513690a16015060c9ad22a582522ae8ee9d6
Adrian Thomas <adrian.l.thomas@outlook.com>
2017-10-24 01:14:25 -0500

    optimizes app entrypoint

--- basic-redux-boilerplate/src/components/app.js
+++ basic-redux-boilerplate/src/components/app.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>hello cruel world!</h1>
+        <h1>hello wonderful world!</h1>
       </div>
     )
   }
--- basic-redux-boilerplate/src/index.js
+++ basic-redux-boilerplate/src/index.js
@@ -9,14 +9,26 @@ import React from 'react';
 import ReactDom from "react-dom";
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { AppContainer } from 'react-hot-loader';
 
 import App from './components/app';
 import reducers from './rootReducer';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDom.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.getElementById('root'));
+const render = Component => {
+  ReactDom.render(
+    <AppContainer>
+      <Provider store={createStoreWithMiddleware(reducers)}>
+        <Component />
+      </Provider>
+    </AppContainer>
+    , document.getElementById('root')
+  );
+}
+
+render(App)
+if (module.hot) {
+  module.hot.accept('./components/app', () => { render(App)
+  });
+}
